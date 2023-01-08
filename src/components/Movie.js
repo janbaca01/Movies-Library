50df58afed2ac6139ca67099b291604d65632131
janbaca01 <janbaca01@gmail.com>
2023-01-08 23:04:26 +0100

    základné naštylovanie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,24 @@
-import React, { Component }  from 'react';
 import './Movie.css'
 import data from '../Data'
 
 
 const Movie = () => {
     return (
-            <div>
-                {
-                    data.map( (oneMovie) => {
-                        const {id, image, title, age, tags, description} = oneMovie
+        <>
+            {
+                data.map( (oneMovie) => {
+                    const {id, image, title, age, tags, description} = oneMovie
 
-                        return <div key={id}>
-                            <img src={image} alt="" />
-                            <h2>{title}</h2>
-                            <p>{age}</p>
-                            <p>{tags}</p>
-                            <p>{description}</p>
-                        </div>
-                    })
-                }
-            </div>
+                    return <div key={id} className="one-movie">
+                        <img src={image} alt="" />
+                        <h2>{title}</h2>
+                        <p>{age}</p>
+                        <p>{tags}</p>
+                        <p>{description}</p>
+                    </div>
+                })
+            }
+        </>
     )
 }
 
